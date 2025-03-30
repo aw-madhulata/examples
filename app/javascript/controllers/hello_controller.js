f3c44a22760e82441b8f1b1075d385d03bff92b7
Awijeet Shandilya <aw@madhulata.org>
2025-03-30 14:03:12 +0530

    Code update

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -2,20 +2,67 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = ["name", "modal"]
+  static values = { chartParam: String }; // Define chartParamValue
   connect() {
     // this.element.textContent = "Hello World!"
-    console.log("Hello World!");
+    // console.log("Hello World!");
     this.modal = document.getElementById("modal");
+    // this.chart();
   }
   greet(){
     console.log(`Hello ${this.nameTarget.value}!`)
   }
 
-  open() {
+  open(event) {
+    this.chart(event);
     this.modal.classList.remove("hidden");
   }
 
   close() {
     this.modal.classList.add("hidden");
   }
+  chart(event) {
+    // console.log("Stimulus received click event.");
+    let type = event.target.dataset.helloChartParam || "daily";
+    // console.log("Selected chart type:", type);
+    // let type = this.hasChartParamValue ? this.chartParamValue : 'daily'; // Default to 'daily'
+  
+    let data;
+    if (type === 'daily') {
+      data = google.visualization.arrayToDataTable([
+        ['Task', 'Hours per Day'],
+        ['Work', 11],
+        ['Eat', 2],
+        ['Commute', 2],
+        ['Watch TV', 2],
+        ['Sleep', 7]
+      ]);
+    } else if (type === 'weekly') {
+      data = google.visualization.arrayToDataTable([
+        ['Task', 'Hours per Week'],
+        ['Work', 55],
+        ['Eat', 14],
+        ['Commute', 10],
+        ['Watch TV', 10],
+        ['Sleep', 49]
+      ]);
+    } else if (type === 'monthly') {
+      data = google.visualization.arrayToDataTable([
+        ['Task', 'Hours per Month'],
+        ['Work', 240],
+        ['Eat', 60],
+        ['Commute', 40],
+        ['Watch TV', 50],
+        ['Sleep', 210]
+      ]);
+    }
+  
+    let options = {
+      title: `My ${type.charAt(0).toUpperCase() + type.slice(1)} Activities`,
+    };
+  
+    var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+    chart.draw(data, options);
+  }
+  
 }
